ecad908055aee7f153ea27d58b7724dd758ab25e
rochar@gmail.com <rochar@gmail.com>
2022-03-30 11:02:32 +0100

    add tools json data

--- src/components/tools.js
+++ src/components/tools.js
@@ -1,13 +1,47 @@
 import React from "react";
-
+import axios from "axios";
+import Spinner from "./spinner";
 class Tools extends React.Component {
-  state = { toolsFamily: null };
+  state = { tools: [], error: null };
+
+  componentDidMount() {
+    axios
+      .get("./data/toolsdata.json")
+      .then((response) => {
+        this.setState({ tools: response.data, error: null });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ tools: [], error: error });
+      });
+  }
+
+  conditionalRender() {
+    const { tools, error } = this.state;
+
+    if (error !== null) {
+      return <div>Could not load tools: {error}</div>;
+    }
+     if (tools.length > 0) {         
+       const toolsComponents = tools.map((tool) => (
+            <div key={tool.url}>
+                <a href={tool.url} target="_blank">{tool.title}</a>
+                <div>{tool.description}</div>
+                </div>       
+       ));
+       return <div >{ toolsComponents }</div>;
+     }
+
+    return <Spinner />;
+  }
+
   render() {
     return (
-      <form>        
-          <select className="form-select">
+      <form>
+        {/* <select className="form-select">
           <option value="1">One</option>
-          </select>
+        </select> */}
+        <div>{this.conditionalRender()}</div>
       </form>
     );
   }
