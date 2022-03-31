6e09b43e24b011b9cf497b5e2f378a4e38d1995e
rochar@gmail.com <rochar@gmail.com>
2022-03-31 09:48:27 +0100

    populate tools familiy options

--- src/components/tools.js
+++ src/components/tools.js
@@ -2,48 +2,77 @@ import React from "react";
 import axios from "axios";
 import Spinner from "./spinner";
 class Tools extends React.Component {
-  state = { tools: [], error: null };
+  state = { toolsData: [], families: [], error: null };
 
   componentDidMount() {
     axios
       .get("./data/toolsdata.json")
       .then((response) => {
-        this.setState({ tools: response.data, error: null });
+        const unqiueFamilies = this.computDistinctFamily(response.data);
+        this.setState({
+          toolsData: response.data,
+          families: unqiueFamilies,
+          error: null,
+        });
       })
       .catch((error) => {
         console.log(error);
-        this.setState({ tools: [], error: error });
+        this.setState({ toolsData: [], families: [], error: error });
       });
   }
+  rendeFamilies() {
+    const { toolsData, families, error } = this.state;
 
-  conditionalRender() {
-    const { tools, error } = this.state;
+    if (error !== null) {
+      return;
+    }
+    const components = families.map((family) => <option key={family}>{family}</option>);
+    return (
+      <select className="form-select" onChange={this.onSelectedFamilyChanged}>
+        {components}
+      </select>
+    );
+  }
+  renderListTools() {
+    const { toolsData, families, error } = this.state;
 
     if (error !== null) {
       return <div>Could not load tools: {error}</div>;
     }
-     if (tools.length > 0) {         
-       const toolsComponents = tools.map((tool) => (
-            <div key={tool.url}>
-                <a href={tool.url} target="_blank">{tool.title}</a>
-                <div>{tool.description}</div>
-                </div>       
-       ));
-       return <div >{ toolsComponents }</div>;
-     }
+    if (toolsData.length > 0) {
+      const components = toolsData.map((tool) => (
+        <div key={tool.url} className="p-2">
+          <a href={tool.url} target="_blank">
+            {tool.title}
+          </a>
+          <div>{tool.description}</div>
+        </div>
+      ));
+      return <div>{components}</div>;
+    }
 
     return <Spinner />;
   }
 
   render() {
     return (
+      <div className="p-3">
       <form>
-        {/* <select className="form-select">
-          <option value="1">One</option>
-        </select> */}
-        <div>{this.conditionalRender()}</div>
+        {this.rendeFamilies()}
+        <div>{this.renderListTools()}</div>
       </form>
+      </div>
     );
+  }
+
+  onSelectedFamilyChanged(event) {
+    console.log(event.target.value);
+  }
+
+  computDistinctFamily(toolsData) {
+    const uniqueValues = new Set();
+    for (const tool of toolsData) uniqueValues.add(tool.family);
+    return Array.from(uniqueValues);
   }
 }
 
