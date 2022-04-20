8f2b43b545fbf3d990c945dd8aa91a5596f0b91f
rochar@gmail.com <rochar@gmail.com>
2022-04-20 09:07:38 +0100

    add call cuda from managed code article

--- src/components/articles/cudakernelsfrommanagedcode.js
+++ src/components/articles/cudakernelsfrommanagedcode.js
@@ -3,201 +3,91 @@ import Breadcrumbs from "../breadcrumbs";
 import ArticleInfo from "./articleinfo";
 import Gist from "react-gist";
 
-//import ArticleImage from "../../assets/img/articles/optimizememallocation.jpg";
+import ArticleImage from "../../assets/img/articles/cudakernelsfrommanagedcode1.png";
+import ArticleImage2 from "../../assets/img/articles/cudakernelsfrommanagedcode2.png";
+import ArticleImage3 from "../../assets/img/articles/cudakernelsfrommanagedcode3.png";
+import ArticleImage4 from "../../assets/img/articles/cudakernelsfrommanagedcode4.png";
+import ArticleImage5 from "../../assets/img/articles/cudakernelsfrommanagedcode5.png";
+
+import ArticleImageKeepItClean from "../../assets/img/articles/avoidrelaycommands.jpg";
+
+
 
 const Article = () => {
   const summary =
-    "Multiple Relay Commands instances can become a source of serious CPU usage for long living View Models in a WPF application using MVVM pattern.";
+    "Tutorial on how to call CUDA Kernels from a .Net (C#) library without any third party dependencies, “simple and direct”!";
   // <!-- ======= Portfolio Details Section ======= -->
   return (
     <React.Fragment>
-      <Breadcrumbs title="Optimize Memory Allocation in a Producer / Consumer Pattern (C#)" />
+      <Breadcrumbs title="Call CUDA kernels from Managed Code (.Net C#) in a simple way" />
       <section id="portfolio-details" className="portfolio-details">
         <div className="container">
           <div className="row gy-4">
             <div className="col-lg-8">
               <h2>Introduction</h2>
               <p>
-                When using MVVM pattern in a WPF application we usually create
-                custom ICommand implementations, one of them is known as Relay
-                Command. If you are unfamiliar with commands a good starting
-                point is{" "}
-                <a
-                  href="https://docs.microsoft.com/en-us/dotnet/desktop/wpf/advanced/commanding-overview?view=netframeworkdesktop-4.8"
-                  target="_blank"
-                >
-                  here
-                </a>
-                .
+              This is a small tutorial on how easily we can call a CUDA Kernel from a .Net library without relying on third party or any complex code. Its simple, direct, easy to debug and maintain, but the trade-off is you need to implement a “new entry point” to each new kernel you add.
               </p>
-              <p>
-                I have worked in several WPF applications that had peaks on CPU
-                usage, and profiling the application, using{" "}
-                <a href="https://www.jetbrains.com/profiler/" target="_blank">
-                  dotTrace
-                </a>
-                &nbsp;profile for example, we could see several millions of
-                calls to the CanExecute method. This type of commands are spread
-                around the application and have significant impact for long
-                living View Models, like the “Root” View Model, that could have
-                hundred of commands, for example: in menus, context menus ,
-                toolbars and other input controls. In this article I explain why
-                Relay Commands may have a performance hit and I give some clean
-                and simple solutions to solve it.
-              </p>
-              <h2> Relay Commands</h2>
-              The first timed I read about Relay Commands was in{" "}
-              <a
-                href="https://github.com/lbugnion/mvvmlight/blob/master/GalaSoft.MvvmLight/GalaSoft.MvvmLight%20(PCL)/Command/RelayCommand.cs"
-                target="_blank"
-              >
-                MVVM Light Toolkit
-              </a>
-              , since then I have seen several implementation examples like this
-              <a
-                href="https://www.technical-recipes.com/2016/using-relaycommand-icommand-to-handle-events-in-wpf-and-mvvm/"
-                target="_blank"
-              >
-                {" "}
-                one
-              </a>
-              . So let us look to an usual Relay Command implementation in the
-              next snippet:
-              <Gist id="6229f82df53f2856a49ed8b1595fbcf5"></Gist>
-              <p>
-                To create an instance (see the constructor) you only need to
-                implement the action for the execute parameter(code to execute
-                when a user clicks a button for example) and the action for the
-                canExecute (state defining if the command can be executed) and
-                that is the main advantage of it, simple and fast to implement.
-              </p>
-              <p>A good example is the print command:</p>
-              <Gist id="0e78983201513a4590116712a6dc5af0"></Gist>
-              <p>
-                {" "}
-                The print command command can be executed when the print service
-                is online and just prints when invoked, simple as that, no need
-                to explicitly raise the CanExecuteChanged event(will explain
-                later why) to notify the UI to refresh / invalidate the current
-                state of the inputs associated and therefore no need to think or
-                implement the logic that can change the CanExecute state. In
-                many cases this is an advantage because you can quickly
-                implement the command. If command depends on third party
-                services or data outside of our domain, that we do not own.
-                Probably that third party service is not able to notify the
-                state changed, so we need to continuously querying it, in this
-                case the relay command fits well.
-              </p>
-              <p>
-                <strong>
-                  Why don’t we need to call explicitly the CanExecuteChanged
-                  EventHandler?
-                </strong>
-              </p>
-              <p>Looking to the following snippet of the Relay Command:</p>
-              <Gist id="ca4f05673c26800d9c595b9fae40b106"></Gist>
-              <p>
-                Whenever the CanExecuteChanged is subscribed in fact it’s
-                subscribing the CommandManager.RequerySuggested, and this is the
-                root cause of the performance issue that we may face:
-              </p>
-              <figure className="text-end">
-                <blockquote className="blockquote">
-                  <p>
-                    Occurs when the{" "}
-                    <a
-                      href="https://docs.microsoft.com/en-us/dotnet/api/system.windows.input.commandmanager?view=netframework-4.8"
-                      target="_blank"
-                    >
-                      {" "}
-                      CommandManager{" "}
-                    </a>{" "}
-                    detects conditions that might change the ability of a
-                    command to execute
-                  </p>
-                </blockquote>
-                <figcaption className="blockquote-footer">
-                  Microsoft documentation{" "}
-                  <cite title="Source Title">CommandManager Class</cite>
-                </figcaption>
+              <p>I assume that you already have the basic knowledge for CUDA Programming and .Net. In this tutorial we are using .Net 6.0 (Core), the steps are the same for .Net Framework. All source code can be found <a href="https://github.com/rochar/simplecudanet/tree/1.0" target="_blank">here</a>.</p>
+              <hr></hr>
+              <h2>Step 1 — Install CUDA Toolkit</h2>
+              <p>The latest version can be found here <a href="https://developer.nvidia.com/cuda-downloads" target="_blank">CUDA Toolkit 11.6 Update 1 Downloads | NVIDIA Developer</a>. You can also see the <a href="https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html#build-customizations-for-existing-projects"> Installation Guide Windows :: CUDA Toolkit Documentation (nvidia.com)</a></p>
+              <p>When the installation is completed open the command line and type: nvcc -v</p>
+              <p>You should see something like this (you may need to restart the machine):</p>
+              <figure className="text-center">
+                <img src={ArticleImage} alt="Checking CUDA is properly installed." />
+                <figcaption className="figure-caption">Checking CUDA is properly installed.</figcaption>
               </figure>
-              <p>
-                It’s quite difficult to have an overview when this event is
-                raised, because its trigger outside the CommandManager
-                boundaries, you can dig the dotnet/WPF{" "}
-                <a target="_blank" href="https://github.com/dotnet/wpf">
-                  {" "}
-                  repository{" "}
-                </a>
-                . If you add a break point to the CanExecute method you will see
-                this is called very very very often. A mouse move is sufficient
-                to trigger this event!
-              </p>
-              <h2>Commands that can always be executed</h2>
-              <p>
-                Look to your commands and do not get surprised if a considerable
-                part of then returns always true on the CanExecute method. An
-                easy solution is to implement a command that simple returns true
-                and completely avoid the Relay Command:
-              </p>
-              <Gist id="7f824b8a18445405024e8904cfb42655"></Gist>
-              <p>With this change you easily save a lot of CPU usage.</p>
-              <h2>Action or Delegate Commands</h2>
-              <p>
-                To remove the dependency to CommandManager.RequerySuggested, we
-                need to code the rules that can change the CanExecute state. To
-                do that we replace the Relay Command for a very similar Action
-                Command:
-              </p>
-              <Gist id="50a417af8354992895de12d91f756fb3"></Gist>
-              <p>
-                The main difference is that there is not subscription to
-                CommandManager.RequerySuggested. The previous PrintCommand
-                example needs to be change to:
-              </p>
-              <Gist id="a1a885df1bce1b6017208b469a00a7b5"></Gist>
-              <p>
-                When IsOnlineChanged is raised, in PrintService, we need to
-                explicitly call the RaiseCanExecuteChanged, in order to notify
-                the UI to refresh according to the new state of the command.
-                This small change may reduce the number of calls to CanExecute
-                method to only one call as long asthe PrintService state does
-                not change and this is much more optimized comparing to the
-                Relay Command.
-              </p>
-              <h2>Avoid logic inside CanExecute</h2>
-              <p>
-                Another source of performance issues is that we tend to compute
-                the CanExecute state inside the method itself. If this
-                computation is expensive, and called multiple times, although
-                the state did not change (the command can be attached to
-                multiple inputs), it will have a performance hit. To resolve
-                this issue we just need to compute it to a local variable and
-                then return always the local variable value:
-              </p>
-              <Gist id="cbfb8192e9aced3ace6311da41e6e5f2"></Gist>
-              <h2>Conclusion</h2>
-              <p>
-                If you are developing a application and not a POC avoid the
-                usage of Relay Commands or similar, this will save you for sure
-                a lot of rework. Unless it’s used in very specif situations:
-                short living view models or has dependencies on data that needs
-                to be queried continuously.
-              </p>
-              <p>
-                In my next post I will show how can we improve this
-                implementation using Reactive Extensions, so keep in touch.
-              </p>
+              <h2>Step 2 — Setup C++/CLI project in Visual Studio</h2>
+              <p>I will start by creating a CLR Class Library, to be able to mix managed code and unmanaged.</p>
+              <p>For more details see <a href="https://docs.microsoft.com/en-us/cpp/dotnet/mixed-native-and-managed-assemblies?view=msvc-170" target="_blank"> Mixed (Native and Managed) Assemblies | Microsoft Docs</a></p>
+              <p>Open Visual Studio, in languages select C++ and search by clr, you should see something like this:</p>
+              
+              <figure className="text-center">
+                <img src={ArticleImage2} alt="Create a new CLR class library." />
+                <figcaption className="figure-caption">Create a new CLR class library.</figcaption>
+              </figure>
+              <p>Select CLR Class Library (&lt;the .Net Version that you intend to target&gt;) and create the project.</p>
+              <p> Open the Project Properties and in Configuration Properties &gt; Advanced &gt; C++/CLI Properties you can confirm the .Net Version that is being targeted.</p>
+              <figure className="text-center">
+                <img src={ArticleImage3} alt="Target .Net 6.0." />
+                <figcaption className="figure-caption">Target .Net 6.0.</figcaption>
+              </figure>
+              <p>Next add CUDA as target, in Build Dependencies &gt; Build Customizations select CUDA:</p>
+              <figure className="text-center">
+                <img src={ArticleImage4} alt="Add CUDA targets." />
+                <figcaption className="figure-caption">Add CUDA targets.</figcaption>
+              </figure>
+              <p>Last step link the cudart.lib. For that open the project properties and in Configuration Properties &gt; Linker &gt; Input &gt; Additional Dependencies add cudart.lib:</p>
+              <figure className="text-center">
+                <img src={ArticleImage5} alt="Link the cudart.lib." />
+                <figcaption className="figure-caption">Link the cudart.lib.</figcaption>
+              </figure>
+              <h2>Step 3 — Create the first CUDA Kernel</h2>
+              <p>In this step we will create a simple kernel to add two vector in the GPU. Lets start by adding an header <i>AddVectorsKernel.cuh</i> , in this header the RunAddVectorsKernel is defined and is the C++, “unmanaged entry point” to call the Kernel.</p>
+              <Gist id="cc64b5f673b1179a921507080954b365"></Gist>
+              <p>Next, let’s create the kernel <i>AddVectorsKernel.cu</i></p>             
+              <Gist id="47fab677f2774dd5d943e4dafdabf0fa"></Gist>
+              <p>Now lets add the “managed entry point” for the kernel, the header AddVectorsKernel.h and the cpp <i>AddVectorsKernel.cpp</i>.</p>
+              <Gist id="2ac8ecfc33af01a6b94ce9b90b372879"></Gist>
+              <p>In the header I define a managed static class and a static function that will be called by the managed library. This function accepts two managed arrays and outputs the result of adding both also as managed array.</p>
+              <Gist id="400703f3afe71387074d978cac9e6aa4"></Gist>
+
+              <h2>Step 4— Call the Kernel from .Net Console App</h2>
+              <p>Now Create a Console App, and reference the C++/CLI project created in Step 2. To call the Kernel just past the code:</p>
+              <Gist id="c9543596e2a842140a0e38e100baa4af"></Gist>
+              <p>This is a simple solution, easy to maintain and evolve and can be a good solution when you just want to call CUDA Kernels and have a “manage API” in top of it. In a future I will improve this solution and remove some boilerplate code an make our life easier.</p>
               <p>See you soon.</p>
-              {/* <figure className="text-center">
-                <img src={ArticleImage} alt="Keep it simple. Keep it clean." />
-                <figcaption className="figure-caption">Keep it simple. Keep it clean.</figcaption>
-              </figure> */}
+              <figure className="text-center">
+                <img src={ArticleImageKeepItClean} alt="Keep it Clean, Keep it Simple." />
+                <figcaption className="figure-caption">Keep it Clean, Keep it Simple.</figcaption>
+              </figure>
             </div>
             <ArticleInfo
-              category="C#"
+              category="C#, CUDA"
               date="Apr 14, 2020"
               summary={summary}
+              githuburl="https://github.com/rochar/simplecudanet/tree/1.0"
             ></ArticleInfo>
           </div>
         </div>
