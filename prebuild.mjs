df84b2cc6313915576a85c1d27411df0d459f2ed
rochar@gmail.com <rochar@gmail.com>
2022-04-21 09:25:05 +0100

    update sitemap.xml lastmod on build

--- prebuild.mjs
+++ prebuild.mjs
@@ -1,22 +1,22 @@
 import regexReplace from "regex-replace"
  
 const now = new Date();
+const dateOnly = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
 
-const searchString = '\\d{2}/\\d{2}/\\d{4}';
-const replaceString = now.toLocaleDateString();
-const path = '.\\src\\configuration.json';
- 
+console.log(dateOnly);
+
 const options = {
     filenamesOnly: false, //default
     fileContentsOnly: false //default
 };
 
-const doRegexReplace = async function() {
+const doRegexReplace = async function(searchString,replaceString,path,options) {
     try {
         await regexReplace(searchString, replaceString, path, options);
     } catch (err) {
         console.error('err > regexReplace > testing', err);
     }
 }
-doRegexReplace();
+doRegexReplace('\\d{4}-\\d{1,2}-\\d{1,2}',dateOnly, '.\\src\\configuration.json',options);
+doRegexReplace('\\d{4}-\\d{1,2}-\\d{1,2}',dateOnly, '.\\public\\sitemap.xml',options);
 //callback (no support for callbacks currently)
